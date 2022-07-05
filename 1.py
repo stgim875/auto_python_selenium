baecae75184b327d46ee8ab543b1ccf0cd2b9f6d
stgim875th <stgim@virnect.com>
2022-07-05 18:29:24 +0900

    업데이트

--- 1.py
+++ 1.py
@@ -90,13 +90,22 @@ time.sleep(30)
 # remotecreatebtn = driver.find_element(
 #     By.XPATH, "//div[@class ='workspace-wrapper']/section[@class='workspace-welcome']/div[@class='workspace-welcome__body offsetwidth']/button[@class='btn' and contains(text(), '원격 협업 생성')]").click()
 
+# 원격 협업 생성하기 모달창에서 멤버 선택하여 추가하기
+
+# createroommanager = driver.find_element(
+# By.XPATH, "//*[@class = 'collapsible__content opend']/div[@class = 'widecard choice'][1]").click()
+
+# createroommanager = driver.find_elements(
+#     By.XPATH,
+# "//*[@class= 'collapsible__content opend']/div[@class = 'widecard choice'][1]").click()
+
+createroommanager = driver.find_element(
+    By.XPATH,
+    "//*[@class = 'collapsible__content opend']/div[1]").click()
+
 # 오픈 룸 생성
 # openroomcreatebtn = driver.find_element(
 #     By.XPATH, "//*[@class = 'btn workspace-welcome__open']").click()
-
-# 
-createroommanager = driver.find_element(
-By.XPATH, "//*[@class = 'collapsible__content opend']/div[@class = 'widecard choice'][1]").click()
 
 # 로그아웃 시 매우 중요함
 # status 메뉴 선택
